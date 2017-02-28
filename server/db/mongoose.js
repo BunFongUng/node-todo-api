ebdf4dcf67d8882687bbe1c4c6f65786cace973c
Lester <lester.ung@pathmazing.com>
2017-02-28 20:08:04 +0700

    add mongodb ui

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,6 +3,11 @@ const mongoose = require('mongoose');
 // config mongoose to use promise instead of callback
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost:27017/TodoAppApi');
+if (process.env.NODE_ENV === 'production') {
+    var mongodbUI = 'mongodb://todo_user:123456@ds163679.mlab.com:63679/todoapi';
+} else if (process.env.NODE_ENV === 'local') {
+    var mongodbUI = 'mongodb://localhost:27017/TodoAppApi';
+}
+mongoose.connect(mongodbUI);
 
 module.exports = { mongoose };
