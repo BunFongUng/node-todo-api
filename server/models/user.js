7330d466ae7d00051eaa3a7eff66452d78580fe9
Lester <lester.ung@pathmazing.com>
2017-03-21 21:54:23 +0700

    add delete token

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 let userSchema = new mongoose.Schema({
     email: {
@@ -55,6 +56,25 @@ userSchema.statics.findByToken = function(token) {
     });
 }
 
+userSchema.statics.findByCredentials = function(email, password) {
+    let User = this;
+    return User.findOne({ email }).then((user) => {
+        if (!user) {
+            return Promise.reject('User not found!');
+        }
+
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, res) => {
+                if (res) {
+                    resolve(user);
+                } else {
+                    reject('Invalid password');
+                }
+            });
+        });
+    });
+};
+
 userSchema.methods.generateAuthToken = function() {
     let user = this;
     let access = 'auth';
@@ -70,6 +90,33 @@ userSchema.methods.generateAuthToken = function() {
         return token;
     });
 }
+
+userSchema.methods.removeToken = function(token) {
+    let user = this;
+
+    return user.update({
+        $pull: {
+            tokens: {
+                token
+            }
+        }
+    });
+
+};
+
+userSchema.pre('save', function(next) {
+    let user = this;
+    if (user.isModified('password')) {
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(user.password, salt, (err, hash) => {
+                user.password = hash;
+                next();
+            });
+        });
+    } else {
+        next();
+    }
+});
 
 var User = mongoose.model('User', userSchema);
 
