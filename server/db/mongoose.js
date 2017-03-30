fed26875e3463a1464e6a48e1013959a055baa11
Lester <lester.ung@pathmazing.com>
2017-03-30 21:19:31 +0700

    +

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,11 +3,12 @@ const mongoose = require('mongoose');
 // config mongoose to use promise instead of callback
 mongoose.Promise = global.Promise;
 
-if (process.env.NODE_ENV === 'production') {
-    var mongodbUI = 'mongodb://todo_user:123456@ds163679.mlab.com:63679/todoapi';
-} else {
-    var mongodbUI = 'mongodb://localhost:27017/TodoAppApi';
-}
-mongoose.connect(mongodbUI);
+// if (process.env.NODE_ENV === 'production') {
+//     var mongodbUI = 'mongodb://todo_user:123456@ds163679.mlab.com:63679/todoapi';
+// } else {
+//     var mongodbUI = 'mongodb://localhost:27017/TodoAppApi';
+// }
+
+mongoose.connect(process.env.MONGOURI);
 
 module.exports = { mongoose };
